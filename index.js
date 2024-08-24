8eb799218e8794b89878468d61710a861fc19099
unknown <creator.anirudhsingh@gmail.com>
2024-08-24 23:10:01 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+// Setting EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+// Configuring Static Files
+app.use('public/', express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/:location?', (req, res) => {
+    const location = req.params.location ?? 'home';
+    const file = path.join(__dirname, 'views', `${location}.ejs`);
+    
+    if (fs.existsSync(file)) {
+        return res.render(location);
+    }
+    
+    return res.status(404).render('404');
+});
+
+app.listen(3000, () => {
+    console.log('Preview : http://127.0.0.1:3000');
+});
